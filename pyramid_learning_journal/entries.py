bc0fa9019fa0aa92f6d1f2107a5ecad3aaacdf98
Chris Closser <chrisclosser1@gmail.com>
2017-11-01 17:55:06 -0700

    merge to jinja2 templating

---
+++ pyramid_learning_journal/entries.py
@@ -0,0 +1,15 @@
+"""."""
+
+
+ENTRIES = [
+    {
+        'title': "number one",
+        'creation_date': 'beginning of time',
+        'body': "this is not a blog entry"
+    },
+    {
+        'title': "number one",
+        'creation_date': 'beginning of time',
+        'body': "this is not a blog entry"
+    }
+]
--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -3,23 +3,24 @@ from pyramid.response import Response
 import io
 import os
 from pyramid.view import view_config
+from entries import ENTRIES
 HERE = os.path.dirname(__file__)
 
 
 @view_config(route_name='home', renderer='../templates/index.jinja2')
 def list_view(request):
     """Display the list of entries."""
-    return {}
-    # path = os.path.join(HERE, '../templates/index.html')
-    # with io.open(path) as res:
-    #     return Response(res.read())
+    return {'entries': ENTRIES}
 
 
+@view_config(route_name='detail', renderer='../templates/detail.jinja2')
 def detail_view(request):
     """Display a detail view of entry."""
-    path = os.path.join(HERE, '../templates/detail.html')
-    with io.open(path) as res:
-        return Response(res.read())
+    return {
+        'title': 'a title',
+        'creation_date': 'whatever',
+        'body': 'my body'
+        }
 
 
 def create_view(request):
