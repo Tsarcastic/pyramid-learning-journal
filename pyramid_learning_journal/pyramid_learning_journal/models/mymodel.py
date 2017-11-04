b315fb7546554a0238ea8ab324a7f21e71c209d6
Chris Closser <chrisclosser1@gmail.com>
2017-11-03 17:37:55 -0700

    setting up database

--- pyramid_learning_journal/pyramid_learning_journal/models/mymodel.py
+++ pyramid_learning_journal/pyramid_learning_journal/models/mymodel.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    # DateTime
 )
 
 from .meta import Base
@@ -12,7 +13,9 @@ class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
     name = Column(Text)
-    value = Column(Integer)
+    title = Column(Text)
+    body = Column(Text)
+    # creation_date = DateTime.datetime()
 
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
