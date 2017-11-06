3f3d5cdc2bf9cbb56ffe72e7fe04b55f16457979
Chris Closser <chrisclosser1@gmail.com>
2017-11-05 18:27:20 -0800

    create and edit views working

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -3,8 +3,10 @@ from pyramid.response import Response
 import io
 import os
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 from entries import ENTRIES
 from ..models import MyModel
+import datetime
 
 import pdb
 HERE = os.path.dirname(__file__)
@@ -13,7 +15,8 @@ HERE = os.path.dirname(__file__)
 @view_config(route_name='home', renderer='../templates/index.jinja2')
 def list_view(request):
     """Display the list of entries."""
-    entries = request.dbsession.query(MyModel).all()
+    query = request.dbsession.query(MyModel)
+    entries = query.order_by(MyModel.creation_date.desc()).all()
     return {'entries': entries}
 
 
@@ -30,11 +33,30 @@ def detail_view(request):
 @view_config(route_name='new', renderer='../templates/newEntry.jinja2')
 def create_view(request):
     """Display create a list entry."""
+    if request.POST:
+        entry = MyModel(
+            title=request.POST["title"],
+            creation_date=datetime.datetime.now(),
+            body=request.POST["body"]
+        )
+        request.dbsession.add(entry)
+        return HTTPFound(request.route_url('home'))
     return {}
 
 
+@view_config(route_name='edit', renderer='../templates/editEntry.jinja2')
 def update_view(request):
     """Display the update entry."""
-    path = os.path.join(HERE, '../templates/edit.html')
-    with io.open(path) as res:
-        return Response(res.read())
+    ident = int(request.matchdict["id"])
+    entry = request.dbsession.query(MyModel).get(ident)
+    if request.POST:
+        entry.title = request.POST["title"]
+        entry.body = request.POST["body"]
+        request.dbsession.flush()
+        return HTTPFound(request.route_url('home'))
+
+    form_fill = {
+        "title": entry.title,
+        "body": entry.body
+    }
+    return {"entry": form_fill}
